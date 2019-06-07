7b0e436baf824bd89105cafe8c09a7e52237d982
KorablevDO <krablevd@ya.ru>
2019-06-07 09:10:27 +0300

    Домашняя работа №7. Реализация департамента атм - 2.

--- hw07/src/main/java/org/otus/hw07/DepartmentATM.java
+++ hw07/src/main/java/org/otus/hw07/DepartmentATM.java
@@ -1,23 +1,26 @@
 package org.otus.hw07;
 
 import org.otus.hw07.atm.ATM;
+import org.otus.hw07.atm.ATMWS;
+import org.otus.hw07.atm.SnapshotATM;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class DepartmentATM {
-    private List<ATM> atms = new ArrayList<>();
+    private List<ATMWS> atms = new ArrayList<>();
+    private List<SnapshotATM> backup = new ArrayList<>();
 
-    public void addATM(ATM atm){
+    public void addATM(ATMWS atm){
         this.atms.add(atm);
+        this.backup.add(atm.createSnapshotATM());
     }
 
     public int getAllRemainingSumInATMs(){
-        return this.atms.stream().mapToInt(p ->p.getBalance()).sum();
+        return this.atms.stream().mapToInt(ATM::getBalance).sum();
     }
 
     public void restoreOriginalStateATMs(){
-
+        this.backup.forEach(SnapshotATM::restore);
     }
 }
--- hw07/src/main/java/org/otus/hw07/MainClass.java
+++ hw07/src/main/java/org/otus/hw07/MainClass.java
@@ -2,31 +2,32 @@ package org.otus.hw07;
 
 import org.otus.hw07.atm.ATM;
 import org.otus.hw07.atm.ATMFactori;
+import org.otus.hw07.atm.ATMWS;
 import org.otus.hw07.atm.exception.ATMException;
 
 public class MainClass {
     public static void main(String[] args) throws ATMException {
         DepartmentATM departmentATM = new DepartmentATM();
-        ATM atm1 = ATMFactori.standartATM();
+        ATMWS atm1 = ATMFactori.standartATM();
         departmentATM.addATM(atm1);
         System.out.println("atm1: " + atm1.getBalance());
-        ATM atm2 = ATMFactori.standartATM();
+        ATMWS atm2 = ATMFactori.standartATM();
         departmentATM.addATM(atm2);
         System.out.println("atm2: " + atm2.getBalance());
-        ATM atm3 = ATMFactori.containerStorageATM();
+        ATMWS atm3 = ATMFactori.containerStorageATM();
         departmentATM.addATM(atm3);
         System.out.println("atm3: " + atm3.getBalance());
-        ATM atm4 = ATMFactori.containerStorageATM();
+        ATMWS atm4 = ATMFactori.containerStorageATM();
         departmentATM.addATM(atm4);
         System.out.println("atm4: " + atm4.getBalance());
         System.out.println("sum: " + departmentATM.getAllRemainingSumInATMs());
 
         atm1.outputBanknote(1000);
         atm2.outputBanknote(3300);
-        atm2.outputBanknote(350);
+//        atm2.outputBanknote(350);
         atm3.outputBanknote(1000);
         atm4.outputBanknote(3300);
-        atm4.outputBanknote(350);
+//        atm4.outputBanknote(350);
 
 
         System.out.println("sum: " + departmentATM.getAllRemainingSumInATMs());
