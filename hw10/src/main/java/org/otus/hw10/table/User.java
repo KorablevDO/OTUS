c87dc2634d0ca0471c43ce3d56492bcf4a78e799
KorablevDO <krablevd@ya.ru>
2019-06-25 21:06:02 +0300

    Домашняя работа №10. Update class User.

--- hw10/src/main/java/org/otus/hw10/table/User.java
+++ hw10/src/main/java/org/otus/hw10/table/User.java
@@ -1,12 +1,26 @@
 package org.otus.hw10.table;
 
+import javax.persistence.*;
 import java.util.List;
 
+@Entity
+@Table(name = "user")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
+
+    @Column
     private String name;
+
+    @Column
     private int age;
+
+    @OneToOne(mappedBy = "user_id", cascade = CascadeType.ALL)
     private AddressDataSet addressDataSet;
+
+    @OneToMany(mappedBy = "user_id", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<PhoneDataSet> phoneDataSetList;
 
     public User(String name, int age, AddressDataSet addressDataSet, List<PhoneDataSet> phoneDataSetList){
@@ -24,11 +38,19 @@ public class User {
         this.phoneDataSetList =phoneDataSetList;
     }
 
-    public void setId(long id){
-        this.id = id;
+    public void  setName(String name){
+        this.name = name;
     }
 
-    public void  setName(String name){
+    public void setAge(int age){
+        this.age = age;
+    }
+
+    public void setAddressDataSet(AddressDataSet addressDataSet){
+        this.addressDataSet = addressDataSet;
+    }
+
+    public void setPhoneDataSetList(){
 
     }
 
